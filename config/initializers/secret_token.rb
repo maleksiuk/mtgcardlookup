33db8c4f10cce261c3016df14c0f3c739a9df716
Mike Aleksiuk <maleksiuk@gmail.com>
2014-07-01 12:34:31 -0600

    Initial commit -- just empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mtgcardlookup::Application.config.secret_key_base = '9695c4b126079a64d4a348f5d173a7d8f5de48be65c6b3531c6f3a75b06c25e154ef320792d4ccefd4e824bcd80972dee9ba791f0d6a19e3d5dcced0aadbd905'
